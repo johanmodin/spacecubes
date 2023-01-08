0ca8b7086c4579c193e9d31995450b708c74284b
Johan Modin <johan_ahlkvist@hotmail.com>
2023-01-08 09:08:55 +0100

    add writing full numpy frames to terminal

--- terminumpy/io_devices/terminal.py
+++ terminumpy/io_devices/terminal.py
@@ -1,5 +1,4 @@
-
-from collections import defaultdict
+from collections import UserDict
 import curses
 import curses.panel
 
@@ -7,16 +6,15 @@ import numpy as np
 import os
 
 from .io_device import IODevice
-from ..renderers.point_renderer import PointRenderer
+from ..renderers.vector_renderer import VectorRenderer
 
 
 class Terminal(IODevice):
     ''' An I/O device that outputs frame data by writing characters
         to the terminal using the ncurses module
     '''
-
-    def __init__(self, colors=None, *args, **kwargs):
-        self.suggested_renderer = PointRenderer
+    def __init__(self, colors={}, *args, **kwargs):
+        self.suggested_renderer = VectorRenderer
         super(Terminal, self).__init__(*args, **kwargs)
 
         # Curses setup
@@ -35,84 +33,69 @@ class Terminal(IODevice):
             pair_idx = color_idx + 1
             curses.init_pair(pair_idx, 0, color_idx)
             color_to_pair_idx[color_idx] = pair_idx
+        if 'border' in colors:
+            colors[self.renderer.border_value] = curses.color_pair(
+                color_to_pair_idx[colors['border']])
+            del colors['border']
+        else:
+            colors[self.renderer.border_value] = curses.COLOR_CYAN
 
-        # Create a map from value to the correct color pair object
-        self.colors = defaultdict(
-            lambda: curses.color_pair(color_to_pair_idx[curses.COLOR_RED]))
-        self.colors.update({'border': curses.color_pair(color_to_pair_idx[curses.COLOR_CYAN])})
-        if colors is not None:
-            self.colors.update(
-                {
-                    value: curses.color_pair(color_to_pair_idx[color])
-                    for value, color in colors.items()
-                }
-            )
+        # Assign missing values (ints) to any color
+        class ColorDict(UserDict):
+            def __missing__(self, key):
+                color_pair = curses.color_pair(
+                    color_to_pair_idx[(int(key)) % (curses.COLORS)])
+                return color_pair
+        self.colors = ColorDict()
+        self.colors.update(
+            {
+                value: curses.color_pair(color_to_pair_idx[color])
+                for value, color in colors.items()
+            }
+        )
             
-        self.colors[self.renderer.border_value] = self.colors["border"]
-
     def get_resolution(self):
         # Return the _writable_ area of the terminal
         w, h = os.get_terminal_size()
         return (h - 1, w)
-
+    
     def _output_frame_data(self, frame_data, camera):
-        # Concatenate data as the direction is not needed here
-        world_points = np.concatenate([frame_data[d]['world_coordinates'] for d in frame_data])
-        image_points = np.concatenate([frame_data[d]['image_coordinates'] for d in frame_data])
-        values = np.concatenate([frame_data[d]['values'] for d in frame_data])
-
-        # Sort world points by distance so that we can paint the
-        # further points first in order to not overwrite a nearer surface
-        camera_position = np.array([[camera.x, camera.y, camera.z]])
-        point_distances = np.linalg.norm(
-            camera_position - world_points, axis=1)
-        distance_sorting = np.argsort(point_distances)
-        image_points = image_points[distance_sorting]
-        values = values[distance_sorting]
-
-        # Round to nearest integer position
-        image_points = (np.round(image_points)).astype(int)
-
         # Paint points in terminal
-        self.paint_points(image_points, values)
+        self.paint_points(frame_data)
 
-    def paint_points(self, image_points, values):
-        """Paints characters at positions defined by image_points and with
-        colors as defined by corresponding element in values. However,
-        the character used here is just a space and the actual color comes
-        from changing the foreground to the background color and vice versa,
-        thus filling the entire character cell with color.
+    def paint_points(self, frame_data):
+        """ Paints characters into the terminal that with some
+            enthusiasm can be seen as representing the 3d view.
+            This is done by printing strings of the space character
+            that have their background color set to whatever color
+            is assigned to the value of the pixel. This is done to
+            fill the entire character cell and provide a better image.
+
+            For high resolutions, this can get pretty slow.
 
         Args:
-            image_points (np.array): A Nx2 array that consists of N points
-                with (u, v) coordinates. The (u, v) coordinates define the pixel
-                position on the screen on which to draw the point
-            values (np.array): A (N,)-shaped array that consists of one
-                value for every point in image_points. The point and value
-                correspondence is done by index, i.e., image_points[i] will
-                be drawn with the color that corresponds to the value of values[i].
-            camera (Camera): The Camera object that holds the window in which
-                to draw the pixels/characters.
+            frame_data (np.array): The (h, w) numpy array to be displayed
 
         """
 
         # Remove old image contents
         self.stdscr.erase()
 
-        # Paint each image point coordinate that has not already been painted
-        # This is ok since the image points are distance sorted
-        painted_coordinates = set()
-        for i, p in enumerate(image_points):
-            p = (p[0], p[1])
-            if p in painted_coordinates:
-                continue
-            painted_coordinates.add(p)
-            # Find the value of the world point corresponding to the
-            # image frame point p in order to decide color/attributes
-            pair_index = self.colors[values[i]]
-
-            # Add some kind of character at the pixel position
-            self.stdscr.addch(*p, " ", pair_index)
+        # Find where the frame changes color along each row
+        # then print strings that occupy these spaces
+        # This is an optimization over printing every
+        # character individually
+        padded_frame_data = np.pad(frame_data, ((0, 0), (1, 0)), constant_values=-1)
+        color_changes = np.argwhere(
+            padded_frame_data[:, 1:] - padded_frame_data[:, :-1])
+        color_changes = zip(color_changes, color_changes[1:])
+        for from_pos, to_pos in color_changes:
+            value = frame_data[from_pos[0], from_pos[1]]
+            length = (to_pos[0] - from_pos[0]) * frame_data.shape[1] + (to_pos[1] - from_pos[1])
+            self.stdscr.addstr(from_pos[0], from_pos[1], " " * length, self.colors[value])
+        
+        # Optional FPS limiter here as all calculations are done
+        self.limit_fps()
 
         # Draw a box around the screen because it's neat and refresh the panel's contents
         self.stdscr.box()
