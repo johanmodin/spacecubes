bc1a15cd34b285a37add07d89ced1da03cde1f6f
Johan Modin <johan.modin92@gmail.com>
2024-09-07 12:49:03 +0200

    fix lazy importing of io devices

--- spacecubes/io_devices/__init__.py
+++ spacecubes/io_devices/__init__.py
@@ -1,4 +1,27 @@
-__all__ = ["opencv", "raw", "terminal"]
-from .opencv import OpenCV
-from .raw import Raw
-from .terminal import Terminal
+__all__ = ["OpenCV", "Raw", "Terminal"]
+
+import importlib
+
+class LazyLoader:
+    def __init__(self, module_name, class_name):
+        self._module_name = module_name
+        self._class_name = class_name
+        self._class_obj = None
+
+    def _load(self):
+        module = importlib.import_module(f'.{self._module_name}', package='spacecubes.io_devices')
+        self._class_obj = getattr(module, self._class_name)
+
+    def __getattr__(self, item):
+        if self._class_obj is None:
+            self._load()
+        return getattr(self._class_obj, item)
+
+    def __call__(self, *args, **kwargs):
+        if self._class_obj is None:
+            self._load()
+        return self._class_obj(*args, **kwargs)
+
+OpenCV = LazyLoader('opencv', 'OpenCV')
+Raw = LazyLoader('raw', 'Raw')
+Terminal = LazyLoader('terminal', 'Terminal')
